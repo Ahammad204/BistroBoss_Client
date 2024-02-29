b72f410a4ddf2287e0d336f5598841529bbb3947
Ahammad204 <Ahammadcode@gmail.com>
2024-03-01 00:41:25 +0600

    added error page

---
+++ src/Pages/ErrorPage/Error.jsx
@@ -0,0 +1,11 @@
+import error from '../../assets/others/404.gif'
+
+const Error = () => {
+    return (
+        <div className='flex justify-center'>
+            <img src={error} alt="" />
+        </div>
+    );
+};
+
+export default Error;
--- src/Routes/BistroRouter.jsx
+++ src/Routes/BistroRouter.jsx
@@ -3,11 +3,13 @@ import MainLayout from "../Layout/MainLayout";
 import Home from "../Pages/Home/Home/Home";
 import Menu from "../Pages/MenuPage/Menu/Menu";
 import Order from "../Pages/OrderPage/Order/Order";
+import Error from "../Pages/ErrorPage/Error";
 
 export const router = createBrowserRouter([
     {
       path: "/",
       element: <MainLayout></MainLayout>,
+      errorElement: <Error></Error>,
       children:[
         {
             path: '/',

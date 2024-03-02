47ad3536633a3d55ac8d4b3dd64d183b3876f815
Ahammad204 <Ahammadcode@gmail.com>
2024-03-02 09:05:33 +0600

    connect firebase

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,18 @@ import './index.css'
 import { RouterProvider } from "react-router-dom";
 import { router } from './Routes/BistroRouter';
 import { HelmetProvider } from 'react-helmet-async';
+import AuthProvider from './providers/AuthProvider';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
 
-    <HelmetProvider>
-      <div className='max-w-screen-xl mx-auto'>
-        <RouterProvider router={router} />
-      </div>
-    </HelmetProvider>
+    <AuthProvider>
+      <HelmetProvider>
+        <div className='max-w-screen-xl mx-auto'>
+          <RouterProvider router={router} />
+        </div>
+      </HelmetProvider>
+    </AuthProvider>
+
   </React.StrictMode>,
 )
